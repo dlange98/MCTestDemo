2b5d03dbcfe3db9b20b63ba98eadb3152e3f6e76
Dan Lange <dlange@Dans-Machine.zoomtown.com>
2017-12-11 18:36:53 -0500

    attempt to cancel an alert dialog.

--- MCDemoUITests/MCDemoUITests.swift
+++ MCDemoUITests/MCDemoUITests.swift
@@ -18,10 +18,20 @@ class MCDemoUITests: XCTestCase {
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
+        
+        addUIInterruptionMonitor(withDescription: "Location Dialog") { (alert) -> Bool in
+            alert.buttons["Cancel"].tap()
+            return true
+        }
+        
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         let app = XCUIApplication()
+        
         app.launch()
-
+        app.swipeUp()
+        //app.buttons["Request Location"].tap()
+        //app.tap() // need to interact with the app for the handler to fire
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         // Are we on the correct view
         let exists = NSPredicate(format: "exists == true")
